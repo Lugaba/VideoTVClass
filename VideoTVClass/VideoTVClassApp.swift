022e22c4a05ef2aee105bb5e2a822362333195a0
Luca Hummel <luca.hummel12@gmail.com>
2022-04-12 13:51:52 -0300

    Initial Commit

---
+++ VideoTVClass/VideoTVClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoTVClassApp.swift
+//  VideoTVClass
+//
+//  Created by Luca Hummel on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoTVClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
